c04502d6d15872fedfcfc90c9b62888b00c2a9d1
Frank Xu <frank.xu@northbright.com>
2016-10-24 14:38:43 +0800

    Package comment should be of the form 'Package ezdb...'

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 /*
-package ezdb provides the functions to make it easy to use levigo to interact golang apps with leveldb.
+Package ezdb provides the functions to make it easy to use levigo to interact golang apps with leveldb.
 */
 package ezdb
